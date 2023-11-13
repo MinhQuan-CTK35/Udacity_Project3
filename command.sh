e074ae8050c95af4b4b4e5be8db4ccdeff6207f0
MinhQuan-CTK35 <96243700+MinhQuan-CTK35@users.noreply.github.com>
2023-11-13 17:03:35 +0700

    Update code
    Update code after use code build

--- command.sh
+++ command.sh
@@ -16,14 +16,15 @@ kubectl port-forward --namespace default svc/udacity-postgresql 5432:5432 & PGPA
 
 # Connecting Via a Pod
 kubectl exec -it udacity-postgre-postgresql-0 bash 
-PGPASSWORD="jVQXWgZXj6" psql postgres://postgres@udacity-postgresql:5432/postgres -c \l
+PGPASSWORD="p0PE4D1ZwR" psql postgres://postgres@udacity-postgresql:5432/postgres -c \l
 
 kubectl port-forward svc/udacity-postgresql 5432:5432
 
-kubectl port-forward --namespace default svc/udacity-postgresql 5432:5432 &
-    PGPASSWORD=ZWhUiLHn1a psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < ./db/2_seed_users.sql
+kubectl port-forward --namespace default svc/udacity-postgresql 5432:5432 & PGPASSWORD=p0PE4D1ZwR psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < ./db/1_create_tables.sql
+kubectl port-forward --namespace default svc/udacity-postgresql 5432:5432 & PGPASSWORD=p0PE4D1ZwR psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < ./db/2_seed_users.sql
+kubectl port-forward --namespace default svc/udacity-postgresql 5432:5432 & PGPASSWORD=p0PE4D1ZwR psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < ./db/3_seed_tokens.sql
 
-DB_USERNAME=postgres DB_PASSWORD=jVQXWgZXj6 python app.py
+DB_USERNAME=postgres DB_PASSWORD=p0PE4D1ZwR python app.py
 
 # Expose the Backend API to the Internet
 kubectl expose deployment backend-coworking --type=LoadBalancer --name=publicbackend
